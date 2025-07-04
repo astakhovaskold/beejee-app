9c98a0ec664fad196d501ef31b4ac6098482547c
Askold Astakhov <54170773+astakhovaskold@users.noreply.github.com>
2025-07-04 13:43:06 +0300

    Remove template code and add basic JWT API with Postgres

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const logger = require('morgan');
+require('dotenv').config();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const authRouter = require('./routes/auth');
+const tasksRouter = require('./routes/tasks');
+const auth = require('./middleware/auth');
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/auth', authRouter);
+app.use('/tasks', auth, tasksRouter);
 
 module.exports = app;
---
+++ middleware/auth.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = function(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (!token) {
+    return res.sendStatus(401);
+  }
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET || 'secret');
+    req.user = payload;
+    next();
+  } catch (err) {
+    return res.sendStatus(403);
+  }
+};
